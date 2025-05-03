ef446aaa43adf24f9ce4d4c6f4317c7558fad39b
Vittorio Distefano <info@vdistefano.studio>
2025-05-03 19:26:19 +0200

    feat(webcrawler): Add HTML to plain text converter
    - Added htmlToPlainText function for converting HTML to plain text
    - Implemented various string replacements using regular expressions

--- cmd/go-rssagg/webcrawler.go
+++ cmd/go-rssagg/webcrawler.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 func crawler(address string) string {
@@ -18,7 +20,48 @@ func crawler(address string) string {
 		log.Fatalf("Error reading response body: %s", err)
 	}
 
-	log.Print(string(body))
+	// Convert HTML to plain text
+	plainText := htmlToPlainText(string(body))
 
-	return string(body)
+	log.Print(string(plainText))
+
+	return string(plainText)
+}
+
+// Simple HTML to plain text converter
+func htmlToPlainText(html string) string {
+	// Replace line breaks with spaces
+	text := strings.ReplaceAll(html, "\n", " ")
+
+	// Replace common HTML entities
+	text = strings.ReplaceAll(text, "&lt;", "<")
+	text = strings.ReplaceAll(text, "&gt;", ">")
+	text = strings.ReplaceAll(text, "&amp;", "&")
+	text = strings.ReplaceAll(text, "&quot;", "\"")
+	text = strings.ReplaceAll(text, "&nbsp;", " ")
+
+	// Remove script and style tags and their content
+	re := regexp.MustCompile("(?i)<script[^>]*>.*?</script>|<style[^>]*>.*?</style>")
+	text = re.ReplaceAllString(text, "")
+
+	// Replace paragraph and break tags with line breaks
+	text = regexp.MustCompile("(?i)</p>|<br[^>]*>|<div[^>]*>").ReplaceAllString(text, "\n")
+
+	// Add extra line breaks for headings
+	text = regexp.MustCompile("(?i)</h[1-6]>").ReplaceAllString(text, "\n\n")
+
+	// Remove all remaining HTML tags
+	re = regexp.MustCompile("<[^>]*>")
+	text = re.ReplaceAllString(text, "")
+
+	// Collapse multiple spaces
+	text = regexp.MustCompile(`\s{2,}`).ReplaceAllString(text, " ")
+
+	// Collapse multiple line breaks
+	text = regexp.MustCompile(`\n{3,}`).ReplaceAllString(text, "\n\n")
+
+	// Trim leading/trailing whitespace
+	text = strings.TrimSpace(text)
+
+	return text
 }
